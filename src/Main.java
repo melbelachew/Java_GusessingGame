6172bd3db575b88e9fb93f7cf975a7abcc27e528
Mel <melbelachew@gmail.com>
2019-10-22 13:57:35 -0400

    added Random class and updated variable 'myNum'

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
 
-        int myNum = 5;
+        Random r = new Random();
+        int myNum = 1 + r.nextInt(10);
+
+        Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Guess what number I am thinking of between 1 & 10?");
 
